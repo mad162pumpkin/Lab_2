639a5bb400b4da796a22ef31055d88950a32efb2
Dmitriy Sharin <91845078+mad162pumpkin@users.noreply.github.com>
2021-10-03 23:13:02 +0300

    Update Z2.java

--- Z2.java
+++ Z2.java
@@ -2,7 +2,7 @@ package lab_2;
 
 public class Z2 {
     public static void main(String[] args){
-        Animal animal1=new Animal("Мясо","Россия");
+        Animal animal1=new Animal("РњСЏСЃРѕ","Р РѕСЃСЃРёСЏ");
         System.out.println(animal1.makeNoise());
         System.out.println(animal1.eat());
         System.out.println(animal1.sleep());
@@ -12,7 +12,7 @@ public class Z2 {
         System.out.println(animal1.makeNoise());
         System.out.println(animal1.eat());
         System.out.println();
-        Dog animal2=new Dog("Корм","Россия", true);
+        Dog animal2=new Dog("РљРѕСЂРј","Р РѕСЃСЃРёСЏ", true);
         System.out.println(animal2.makeNoise());
         System.out.println(animal2.eat());
         System.out.println(animal2.sleep());
@@ -21,9 +21,9 @@ public class Z2 {
         System.out.println(animal2.unSleep());
         System.out.println(animal2.makeNoise());
         System.out.println(animal2.eat());
-        System.out.println("Собака знает команды?:" + animal2.isKnowCommands);
+        System.out.println("РЎРѕР±Р°РєР° Р·РЅР°РµС‚ РєРѕРјР°РЅРґС‹?:" + animal2.isKnowCommands);
         System.out.println();
-        Cat animal3=new Cat("Что попало","Россия", false);
+        Cat animal3=new Cat("Р§С‚Рѕ РїРѕРїР°Р»Рѕ","Р РѕСЃСЃРёСЏ", false);
         System.out.println(animal3.makeNoise());
         System.out.println(animal3.eat());
         System.out.println(animal3.sleep());
@@ -32,10 +32,10 @@ public class Z2 {
         System.out.println(animal3.unSleep());
         System.out.println(animal3.makeNoise());
         System.out.println(animal3.eat());
-        System.out.println("Кошка приучена к лотку?:" + animal3.isKnowWhereWc);
+        System.out.println("РљРѕС€РєР° РїСЂРёСѓС‡РµРЅР° Рє Р»РѕС‚РєСѓ?:" + animal3.isKnowWhereWc);
         System.out.println();
 
-        Horse animal4=new Horse("Яблоки","Россия", "Скаковая");
+        Horse animal4=new Horse("РЇР±Р»РѕРєРё","Р РѕСЃСЃРёСЏ", "РЎРєР°РєРѕРІР°СЏ");
         System.out.println(animal4.makeNoise());
         System.out.println(animal4.eat());
         System.out.println(animal4.sleep());
@@ -44,7 +44,7 @@ public class Z2 {
         System.out.println(animal4.unSleep());
         System.out.println(animal4.makeNoise());
         System.out.println(animal4.eat());
-        System.out.println("Порода лошади:" + animal4.poroda);
+        System.out.println("РџРѕСЂРѕРґР° Р»РѕС€Р°РґРё:" + animal4.poroda);
         System.out.println();
         Animal[] animals = {animal1, animal2, animal3, animal4};
         Veterinar.treatAnimal(animals);
